38078fa746345f4b495e4b6cd5c23876de45df83
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-10-15 00:59:51 -0500

    Update Problema 2.cpp

--- Tarea-5/Problema 2.cpp
+++ Tarea-5/Problema 2.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************************
+ /***************************************************************************************
  *Problema 2.cpp
  * Programa creado por: Martin Javier Ayala Miranda
  * Fecha de creación: 11/Oct/2015
@@ -7,7 +7,7 @@
  * ALGORITMO PARA OBTENER LA FECHA DEL DÍA SIGUIENTE
  * El algoritmo ,ayuda , dados, el año, el mes y el dia de una fecha, a mostrar la fecha del dia siguiente
  * Entradas: La fecha , año(a),mes (mes) dia,(dia)
- * Parametros: No hay
+ * Parametros: 2 variables una para marcar si el año ingresado es bisiesto(bisiesto) y una para ver si la fecha es valida (valido)
  * Salidas: Se mostrará en pantalla un texto el cual dirá la fecha del dia siguiente en el mismo formato de entrada
 ****************************************************************************************/
  #include <stdio.h>
@@ -15,22 +15,34 @@
  {
  	///Declaracion de variables
  	int a,m,d;
- 	int bisiesto;
+ 	int bisiesto = 0;
+ 	int valido = 1;
  	///Lectura de los datos de entrada
  	printf("Introduzca la fecha en formato Año/Mes/Dia: ");
  	scanf("%d%d%d",&a,&m,&d);
  	if((a % 4 == 0 )||(a % 100 == 0 && a % 400 == 0))
     {
         bisiesto = 1;
-    } else bisiesto = 0;
-
+    }
     if(m > 0 && m < 13 && d > 0 &&  d < 32)
  {
      if(m == 2)
     {
-        if( d > 29 || (d == 29 && bisiesto == 0)) printf("Fecha invalida");
-         if( d == 29 && bisiesto == 1) printf("%d%d%d",a,m + 1,d + 1);
-         if( (d == 28 && bisiesto == 1) || ( d < 28)) printf("%d%d%d",a,m,d+1);
+        if( d > 29 || (d == 29 && bisiesto == 0))
+        {
+        	valido = 0;
+        }
+         if( d == 29 && bisiesto == 1)
+         {
+         	m++;
+         	d = 1;
+         } else
+         {
+             if( (d == 28 && bisiesto == 1) || ( d < 28))
+             {
+                d++;
+             }
+         }
 
      } else
      {
@@ -38,27 +50,34 @@
          {
              if(d == 31)
              {
-                printf("%d%d%d",a,m + 1 ,d = 1);
+                m++;
+                d = 1;
              } else
              {
-                printf("%d%d%d",a,m,d + 1);
-
+                d++;
              }
          } else
          {
            if( m == 12 && d == 31)
            {
-            printf("%d %d %d",a + 1,m = 1, d = 1);
+            a++;
+            d = 1;
+            m = 1;
 
            }else
            {
-             if(d == 30)printf("%d%d%d",a,m + 1, d + 1);
-             else printf ("%d%d%d",a,m,d + 1);
+             if(d == 30)
+             {
+             	m++;
+             	d = 1;
+             }
+             else d++;
            }
 
          }
      }
- } else printf("Fecha no valida");
- 	///printf("La fecha del dia siguiente es: %02d %02d %02d",a,mes,dia);
+ } else valido = 0;
+ 	if(valido == 1) printf("La fecha del dia siguiente es: %02d %02d %02d",a,m,d);
+ 	else printf("Fecha Invalida");
     return 0;
  }
