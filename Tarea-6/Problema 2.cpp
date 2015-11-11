50da76d71c957e8455e50a3f0b694ad06c5ba07b
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-11 18:18:20 -0500

    Create Problema 2.cpp

---
+++ Tarea-6/Problema 2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXN 10000
+int main()
+{
+	int n;
+	int actual;
+	int numbers[MAXN];
+	int big1,big2;
+scanf("%d",&n);
+	for(int i = 0; i<n; i++)
+	{
+		scanf("%d",&numbers [i]);
+	}
+	big1 = numbers [0];
+	for(int i = 1; i<n; i++)
+	{
+		if(big1 < numbers[i])
+		{
+			big1 = numbers[i];
+		}
+	}
+	big2 = numbers[0];
+	for(int i = 1; i<n; i++)
+	{
+		if(big2 < numbers[i]&& numbers[i] != big1)
+		{
+			big2 = numbers[i];
+		}
+	}
+  promedio = (big1 + big2) / 2;
+ printf("%d",promedio);
+ return 0;
+}
