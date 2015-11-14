91b1ea967ca922e94a6e4f6f7f64de46b48da84a
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-13 23:27:47 -0500

    Update Problema 7.cpp

--- Tarea-6/Problema 7.cpp
+++ Tarea-6/Problema 7.cpp
@@ -1,22 +1,35 @@
 #include <stdio.h>
-#define MAXN 10000
+#define MAXN 100
 int main()
 {
+    int numbers[MAXN][MAXN];
+    int m,n;
 	scanf("%d%d",&n,&m);
 
 	for(int i = 0; i<n; i++)
 	{
 		for(int j = 0; j<m; j++)
 		{
-		scanf("%d",&numbers [i]);
-		}	
+		scanf("%d",&numbers[i][j]);
+		}
 	}
+	printf("\n");
+	for(int i = 0; i<n; i++)
+	{
+		for(int j = 0; j<m; j++)
+		{
+		printf("%d ",numbers [i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 	for(int i = 0; i<m; i++)
 	{
 		for(int j = 0; j<n; j++)
 		{
-		printf("%d",numbers [j][i]);
-		}	
+		printf("%d ",numbers [j][i]);
+		}
+		printf("\n");
 	}
  return 0;
 }
