e227430aeb59108a143327e883eced4c04a038a1
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-17 09:37:16 -0500

    Update Problema 1.cpp

--- Tarea-6/Problema 1.cpp
+++ Tarea-6/Problema 1.cpp
@@ -1,4 +1,15 @@
-
+/**********************************************************************************
+*Problema 1.cpp
+ * Programa creado por: Martin Javier Ayala Miranda
+ * Fecha de creación: 15/Nov/2015
+ * Fecha de última modificación: 15/Nov/2015
+ * Descripción:
+ *       ALGORITMO PARA DETERMINAR CUANTOS NUMEROS PARES EXISTEN EN UN ARREGLO
+ *   El algoritmo ,ayuda ,dados n numeros,realizar el conteo de numeros pares existentes*
+ * Entradas: 1 número  (n), n numeros en un arreglo (numbers[])                   
+ * Parametros: Un contador de numeros pares (Pares = 0)
+ * Salidas: Se mostrará en pantalla la cantidad de numeros pares existentes en el arreglo”, 
+**********************************************************************************/
 #include <stdio.h>
 #define MAXN 1000
 int main()
