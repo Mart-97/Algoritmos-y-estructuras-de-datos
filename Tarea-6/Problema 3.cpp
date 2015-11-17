c6d9bbb865b6560dfa11f10c9b873d5dffc823ab
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-17 09:46:11 -0500

    Update Problema 3.cpp

--- Tarea-6/Problema 3.cpp
+++ Tarea-6/Problema 3.cpp
@@ -1,3 +1,15 @@
+/**********************************************************************************
+ *Problema 3.cpp
+ * Programa creado por: Martin Javier Ayala Miranda
+ * Fecha de creación: 15/Nov/2015
+ * Fecha de última modificación: 15/Nov/2015
+ * Descripción:
+ *      ALGORITMO PARA DETERMINAR LOS NUMEROS UNICOS EN UN ARREGLO
+ *El algoritmo ,ayuda ,dados un numero  n de numeros,determina cuales son unicos y los imprime
+ *Entradas: Un numero (n), Un arreglo de numeros(numbers[ ])
+ *Parametros: No hay
+ *Salidas: Un arreglo que contiene a los numeros unicos ( unicos[ ])
+ **********************************************************************************/
 #include <stdio.h>
 #define MAXN 10000
 int main()
