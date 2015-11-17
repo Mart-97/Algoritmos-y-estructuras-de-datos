2357cfc5871acfb3dc0667b3a775b2712f9d89ea
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-17 09:06:06 -0500

    Update Problema 6.cpp

--- Tarea-6/Problema 6.cpp
+++ Tarea-6/Problema 6.cpp
@@ -16,6 +16,7 @@ for(int i = 0; i<n; i++)
 	}
 
 }
+printf("\n");
 for(int i = 0; i<n; i++)
 {
 	for(int j = 0; j < n; j++)
@@ -24,6 +25,7 @@ for(int i = 0; i<n; i++)
 	}
 	printf("\n");
 }
+printf("\n");
 for(int i = n - 1; i >= 0; i--)
 {
 	for(int j = 0; j < n; j++)
