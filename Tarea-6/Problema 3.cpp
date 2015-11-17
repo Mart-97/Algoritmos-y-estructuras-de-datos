4622735dc1e0c2c2bdfca0d68888d4ec0bb90d35
Martín Javier Ayala Miranda <mjayalam@hotmail.com>
2015-11-17 10:18:39 -0500

    Update Problema 3.cpp

--- Tarea-6/Problema 3.cpp
+++ Tarea-6/Problema 3.cpp
@@ -14,7 +14,8 @@
 #define MAXN 10000
 int main()
 {
-	int n;
+	int n; //Variable de entrada
+	///Parametros
 	int pos = 1;
 	 int existe = 0;
 	int unicos[MAXN];
@@ -30,14 +31,17 @@ unicos[0] = numbers[0];
 	{
 		for(int j = 0; j<pos; j++ )
 		{
+			///Verifico si el numero ya existe en el arreglo de numeros unicos
  			if(numbers[i] == unicos[j]) existe = 1;
 		}
 		if(existe != 1)
 		{
+			///Si existe lo almacena en el segundo arreglo e incrementa la cantidad de numeros
 			unicos[pos] = numbers[i];
 			pos++;
 
 		}
+		///Actualizamos nuestro indicador de numeros repetidos
 		existe = 0;
 	}
 	for(int i = 0; i<pos; i++)
